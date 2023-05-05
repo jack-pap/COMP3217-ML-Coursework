34bb9e0af26cad1227e5ba1b5bf6b8458abc463b
Jack Papaioannou <jp1g19@soton.ac.uk>
2023-05-05 17:14:10 +0100

    Added another matrix for training data, typo corrections

--- MLScript.py
+++ MLScript.py
@@ -22,21 +22,20 @@ print (Y_training.shape)
 scaler = StandardScaler()
 X_training = scaler.fit_transform(X_training)
 
-X_training, X_testing, Y_training, Y_testing = train_test_split(X_training, Y_training, test_size=0.2)
+X_training, X_validation, Y_training, Y_validation = train_test_split(X_training, Y_training, test_size=0.2)
 
 # Train a random forest classifier model on the training data
 model = RandomForestClassifier()
 model.fit(X_training, Y_training)
 
-# Evaluate the model on the testing data
-Y_predictions = model.predict(X_testing)
-accuracy = np.mean(Y_predictions == Y_testing)
-f1 = f1_score(Y_testing, Y_predictions)
-precision = precision_score(Y_testing, Y_predictions)
-predictions = model.predict(X_testing)
+# Evaluate the model on the validation data set
+Y_predictions = model.predict(X_validation)
+accuracy = np.mean(Y_predictions == Y_validation)
+f1 = f1_score(Y_validation, Y_predictions)
+precision = precision_score(Y_validation, Y_predictions)
 
 print("Validation accuracy:", accuracy)
-print ("F1 Score:",f1_score(Y_testing, predictions, average='macro'))
+print ("F1 Score:",f1_score(Y_validation, Y_predictions, average='macro'))
 print("Precision:", precision)
 
 # Use the trained model to predict labels for the testing data
@@ -44,8 +43,15 @@ X_test = testData.values
 X_test = scaler.transform(X_test)
 y_pred_test = model.predict(X_test)
 
-# Get confusion matrix
-cm = confusion_matrix(Y_testing, predictions, labels=model.classes_)
+# Get confusion matrix for training data
+Y_trainPredictions = model.predict(X_training)
+cm = confusion_matrix(Y_training, Y_trainPredictions, labels=model.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
+disp.plot(cmap=plt.cm.Reds)
+plt.show()
+
+# Get confusion matrix for testing data
+cm = confusion_matrix(Y_validation, Y_predictions, labels=model.classes_)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
 disp.plot(cmap=plt.cm.PuBu)
 plt.show()
