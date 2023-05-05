6d35671c26a2e52eb1ea79eac88f88855387e138
Jack Papaioannou <jp1g19@soton.ac.uk>
2023-05-05 17:38:40 +0100

    added pr curver

--- MLScript.py
+++ MLScript.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, f1_score, precision_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, f1_score, precision_score, precision_recall_curve
 
 # Loads in training and testing data
 trainData = pd.read_csv("TrainingDataBinary.csv")  
@@ -22,7 +22,8 @@ print (Y_training.shape)
 scaler = StandardScaler()
 X_training = scaler.fit_transform(X_training)
 
-X_training, X_validation, Y_training, Y_validation = train_test_split(X_training, Y_training, test_size=0.2)
+# Splits data into training and validation 80% / 20% 
+X_training, X_validation, Y_training, Y_validation = train_test_split(X_training, Y_training, test_size=0.15)
 
 # Train a random forest classifier model on the training data
 model = RandomForestClassifier()
@@ -30,6 +31,7 @@ model.fit(X_training, Y_training)
 
 # Evaluate the model on the validation data set
 Y_predictions = model.predict(X_validation)
+Y_trainPredictions = model.predict(X_training)
 accuracy = np.mean(Y_predictions == Y_validation)
 f1 = f1_score(Y_validation, Y_predictions)
 precision = precision_score(Y_validation, Y_predictions)
@@ -43,14 +45,30 @@ X_test = testData.values
 X_test = scaler.transform(X_test)
 y_pred_test = model.predict(X_test)
 
+# Output precision recall curve for training data
+precision, recall, thresholds = precision_recall_curve(Y_training, Y_trainPredictions)
+plt.plot(recall, precision, color='r')
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.title('Precision-Recall Curve')
+plt.show()
+
 # Get confusion matrix for training data
-Y_trainPredictions = model.predict(X_training)
+
 cm = confusion_matrix(Y_training, Y_trainPredictions, labels=model.classes_)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
 disp.plot(cmap=plt.cm.Reds)
 plt.show()
 
-# Get confusion matrix for testing data
+# Output precision recall curve for validation data
+precision, recall, thresholds = precision_recall_curve(Y_validation, Y_predictions)
+plt.plot(recall, precision, color='b')
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.title('Precision-Recall Curve')
+plt.show()
+
+# Get confusion matrix for validation data
 cm = confusion_matrix(Y_validation, Y_predictions, labels=model.classes_)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
 disp.plot(cmap=plt.cm.PuBu)
