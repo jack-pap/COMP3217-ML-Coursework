4f39968f5f9f79eb735c848ffa0d5ee5d5ef231e
Jack Papaioannou <jp1g19@soton.ac.uk>
2023-05-06 16:26:41 +0100

    Tried different models, split training and validation metrics

--- MLScript.py
+++ MLScript.py
@@ -3,9 +3,10 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, f1_score, precision_score, precision_recall_curve
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, f1_score, precision_score, precision_recall_curve, recall_score, accuracy_score
 
 # Loads in training and testing data
 trainData = pd.read_csv("TrainingDataBinary.csv")  
@@ -23,26 +24,44 @@ scaler = StandardScaler()
 X_training = scaler.fit_transform(X_training)
 
 # Splits data into training and validation 80% / 20% 
-X_training, X_validation, Y_training, Y_validation = train_test_split(X_training, Y_training, test_size=0.15)
+X_training, X_validation, Y_training, Y_validation = train_test_split(X_training, Y_training, test_size=0.16)
 
 # Train a random forest classifier model on the training data
-model = RandomForestClassifier()
+model = RandomForestClassifier() # 0.98 - 0.99 
+#model = KNeighborsClassifier() # 0.90 - 0.94
+#model = DecisionTreeClassifier() # 0.96- 0.98
 model.fit(X_training, Y_training)
+
+# Evaluate the model on the training data set
+Y_trainPredictions = model.predict(X_training)
+accuracy = np.mean(Y_trainPredictions == Y_training)
+f1 = f1_score(Y_trainPredictions, Y_training)
+precision = precision_score(Y_trainPredictions, Y_training)
+recall = recall_score(Y_trainPredictions, Y_training)
+
+print("Training accuracy:", accuracy)
+print ("Training F1 Score:", f1)
+print("Training Precision:", precision)
+print("Training Recall:", recall)
 
 # Evaluate the model on the validation data set
 Y_predictions = model.predict(X_validation)
-Y_trainPredictions = model.predict(X_training)
 accuracy = np.mean(Y_predictions == Y_validation)
 f1 = f1_score(Y_validation, Y_predictions)
 precision = precision_score(Y_validation, Y_predictions)
+recall = recall_score(Y_validation, Y_predictions)
 
-print("Validation accuracy:", accuracy)
-print ("F1 Score:",f1_score(Y_validation, Y_predictions, average='macro'))
-print("Precision:", precision)
+print("\nValidation accuracy:", accuracy)
+print ("Validation F1 Score:", f1)
+print("Validation Precision:", precision)
+print("Validation Recall:", recall)
+
+# Splits the testing data into features and then normalizes it 
+X_test = testData.values
+print (X_test.shape)
+X_test = scaler.transform(X_test)
 
 # Use the trained model to predict labels for the testing data
-X_test = testData.values
-X_test = scaler.transform(X_test)
 y_pred_test = model.predict(X_test)
 
 # Output precision recall curve for training data
@@ -54,18 +73,9 @@ plt.title('Precision-Recall Curve')
 plt.show()
 
 # Get confusion matrix for training data
-
 cm = confusion_matrix(Y_training, Y_trainPredictions, labels=model.classes_)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
 disp.plot(cmap=plt.cm.Reds)
-plt.show()
-
-# Output precision recall curve for validation data
-precision, recall, thresholds = precision_recall_curve(Y_validation, Y_predictions)
-plt.plot(recall, precision, color='b')
-plt.xlabel('Recall')
-plt.ylabel('Precision')
-plt.title('Precision-Recall Curve')
 plt.show()
 
 # Get confusion matrix for validation data
--- MLScriptMulti.py
+++ MLScriptMulti.py
@@ -3,9 +3,10 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, f1_score, precision_score, precision_recall_curve
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, f1_score, precision_score, recall_score
 
 # Loads in training and testing data
 trainData = pd.read_csv("TrainingDataMulti.csv")  
@@ -23,28 +24,47 @@ scaler = StandardScaler()
 X_training = scaler.fit_transform(X_training)
 
 # Splits data into training and validation 80% / 20% 
-X_training, X_validation, Y_training, Y_validation = train_test_split(X_training, Y_training, test_size=0.15)
+X_training, X_validation, Y_training, Y_validation = train_test_split(X_training, Y_training, test_size=0.16)
 
 # Train a random forest classifier model on the training data
-model = RandomForestClassifier()
+model = RandomForestClassifier() # 0.95 - 0.96
+#model = KNeighborsClassifier() # 0.85 - 0.89
+#model = DecisionTreeClassifier() # 0.89- 0.92
 model.fit(X_training, Y_training)
+
+# Evaluate the model on the training data set
+Y_trainPredictions = model.predict(X_training)
+accuracy = np.mean(Y_trainPredictions == Y_training)
+f1 = f1_score(Y_trainPredictions, Y_training, average='micro')
+precision = precision_score(Y_trainPredictions, Y_training, average='micro')
+recall = recall_score(Y_trainPredictions, Y_training, average='micro')
+
+print("Training accuracy:", accuracy)
+print ("Training F1 Score:", f1)
+print("Training Precision:", precision)
+print("Training Recall:", recall)
 
 # Evaluate the model on the validation data set
 Y_predictions = model.predict(X_validation)
-Y_trainPredictions = model.predict(X_training)
 accuracy = np.mean(Y_predictions == Y_validation)
-f1 = f1_score(Y_validation, Y_predictions, average='macro')
+f1 = f1_score(Y_validation, Y_predictions, average='micro')
+precision = precision_score(Y_validation, Y_predictions, average='micro')
+recall = recall_score(Y_validation, Y_predictions, average='micro')
 
-print("Validation accuracy:", accuracy)
-print ("F1 Score:",f1)
+print("\nValidation accuracy:", accuracy)
+print ("Validation F1 Score:", f1)
+print("Validation Precision:", precision)
+print("Validation Recall:", recall)
+
+# Normalize testing data using a scaler
+X_test = testData.values
+print (X_test.shape)
+X_test = scaler.transform(X_test)
 
 # Use the trained model to predict labels for the testing data
-X_test = testData.values
-X_test = scaler.transform(X_test)
 y_pred_test = model.predict(X_test)
 
 # Get confusion matrix for training data
-
 cm = confusion_matrix(Y_training, Y_trainPredictions, labels=model.classes_)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
 disp.plot(cmap=plt.cm.Reds)
@@ -58,4 +78,4 @@ plt.show()
 
 # Output test label predictions to csv file
 testData['LabelPred'] = y_pred_test
-testData.to_csv('TestingResultsBinary.csv', index=False)
+testData.to_csv('TestingResultsMulti.csv', index=False)
